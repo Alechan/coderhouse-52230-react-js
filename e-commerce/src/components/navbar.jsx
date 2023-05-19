43f8e66d977916e9ef6e73319de18ed266ef7328
Alechan <noemail@example.com>
2023-05-19 20:41:57 -0300

    add collapse button to navbar

--- e-commerce/src/components/navbar.jsx
+++ e-commerce/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NavbarMenu = () => {
-    const categories = ['Home', 'About', 'Services', 'Contact'];
+    const categories = ['Categorías', 'Tiendas oficiales', 'Tu tienda', 'Ayuda'];
 
     const createNavBarItem = (category, index) => (
         // Category will be used as the name of the item
@@ -12,9 +12,12 @@ const NavbarMenu = () => {
     );
 
     return (
-        <ul className="navbar-nav">
-            {categories.map(createNavBarItem)}
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                {categories.map(createNavBarItem)}
+            </ul>
+        </div>
+
     );
 };
 
@@ -24,13 +27,30 @@ const NavBarShopName = () => {
     );
 }
 
+const SmallScreenMenuButton = () => {
+    // This button will "magically appear" when shrinking the window size. Shows the menu items when clicked
+    // (No lo pude hacer andar. Le hago click y no aparece el menú. Probé en chrome y firefox)
+    return (
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    )
+};
 const Navbar = () => {
+    // A responsive navbar that shows a list of items in big screens but a button that shows the menu options when clicked in small screens
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <NavBarShopName />
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <NavbarMenu />
-            </div>
+            <SmallScreenMenuButton />
+            <NavbarMenu />
         </nav>
     );
 };
