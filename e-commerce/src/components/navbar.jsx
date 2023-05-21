51be037f15dd0e458b11cd1003e12b31b55fc047
Alechan <noemail@example.com>
2023-05-20 21:30:48 -0300

    use react-bootstrap library instead of using bootstrap directly

--- e-commerce/src/components/navbar.jsx
+++ e-commerce/src/components/navbar.jsx
@@ -1,58 +1,54 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const NavbarMenu = () => {
-    const categories = ['Categorías', 'Tiendas oficiales', 'Tu tienda', 'Ayuda'];
-
-    const createNavBarItem = (category, index) => (
-        // Category will be used as the name of the item
-        // Index will be used by react (todavía no aprendimos cómo)
-        <li className="nav-item" key={index}>
-            <a className="nav-link" href="#">{category}</a>
-        </li>
-    );
-
+function ShopBrand() {
     return (
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                {categories.map(createNavBarItem)}
-            </ul>
-        </div>
-
-    );
-};
-
-const NavBarShopName = () => {
-    return (
-        <a className="navbar-brand" href="#">PororóShop</a>
+        <Navbar.Brand href="#home">
+            PororóShop
+        </Navbar.Brand>
     );
 }
 
-const SmallScreenMenuButton = () => {
-    // This button will "magically appear" when shrinking the window size. Shows the menu items when clicked
-    // (No lo pude hacer andar. Le hago click y no aparece el menú. Probé en chrome y firefox)
-    return (
-        <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span className="navbar-toggler-icon"></span>
-        </button>
-    )
-};
-const Navbar = () => {
-    // A responsive navbar that shows a list of items in big screens but a button that shows the menu options when clicked in small screens
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <NavBarShopName />
-            <SmallScreenMenuButton />
-            <NavbarMenu />
-        </nav>
-    );
-};
+function BurgerIcon() {
+    return <Navbar.Toggle aria-controls="basic-navbar-nav"/>;
+}
 
-export default Navbar;
+function MenuItems() {
+    const Home = () => <Nav.Link href="#home">Home</Nav.Link>;
+    const TuTienda = () => <Nav.Link href="#tu-tienda">Tu tienda</Nav.Link>;
+    const Categorias = () => {
+        return (
+            <NavDropdown title="Ciudades" id="basic-nav-ciudades">
+                <NavDropdown.Item href="#ciudad/parana">Paraná</NavDropdown.Item>
+                <NavDropdown.Item href="#ciudad/concordia">Concordia</NavDropdown.Item>
+                <NavDropdown.Item href="#ciudad/gualeguaychu">Gualeguaychú</NavDropdown.Item>
+                <NavDropdown.Divider/>
+                <NavDropdown.Item href="#ciudad/todo-entre-rios">A todo Entre Ríos</NavDropdown.Item>
+            </NavDropdown>
+        )
+    };
+
+    return <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Home/>
+            <Categorias/>
+            <TuTienda/>
+        </Nav>
+    </Navbar.Collapse>;
+}
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <ShopBrand/>
+                <BurgerIcon/>
+                <MenuItems/>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
