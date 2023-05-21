51fe17224ef62eadf074075ff81e12ebc16f8643
Alechan <noemail@example.com>
2023-05-21 15:30:02 -0300

    add itemlistcontainer.jsx

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import Navbar from './components/navbar';
+import ItemListContainer from "./components/itemlistcontainer";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <ItemListContainer />
     </div>
+
   );
 }
 
---
+++ e-commerce/src/components/itemlistcontainer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+const ItemListContainer = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>¡Bienvenides a la tienda!</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default ItemListContainer;
