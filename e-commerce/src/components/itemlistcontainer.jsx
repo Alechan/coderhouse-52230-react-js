402f16179b7ca3d541fa736813d3e4dbde2995cd
Alechan <noemail@example.com>
2023-05-22 00:59:43 -0300

    add background image

--- e-commerce/src/components/itemlistcontainer.jsx
+++ e-commerce/src/components/itemlistcontainer.jsx
@@ -1,14 +1,17 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import './itemlistcontainer.css';
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>{greeting}</h1>
-                </Col>
-            </Row>
-        </Container>
+        <div className="background-image-container">
+            <Container fluid className="background-image-blur">
+                <Row className="justify-content-center align-items-center">
+                    <Col className="col-greeter">
+                        <h1>{greeting}</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
