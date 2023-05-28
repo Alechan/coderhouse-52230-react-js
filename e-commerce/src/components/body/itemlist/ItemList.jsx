3f595820ab7a09ef66153d3cd0da5ec9aa5752d6
Alechan <noemail@example.com>
2023-05-28 16:12:14 -0300

    use bootstrap-react in ItemList.jsx

--- e-commerce/src/components/body/itemlist/ItemList.jsx
+++ e-commerce/src/components/body/itemlist/ItemList.jsx
@@ -1,12 +1,21 @@
 import Item from "../item/Item";
+import {Col, Container, Row} from "react-bootstrap";
 
 const ItemList = ({items}) => {
 
     return (
         <div className="item-list">
-            {items.map((item) => (
-                <Item item={item}/>
-            ))}
+            <Container fluid className="item-list-container">
+                <Row className="justify-content-center">
+                    {items.map((item, index) => (
+                        // TODO: Poner el index para que no me tire un warning
+                        <Col key={index} >
+                            <Item item={item}/>
+                        </Col>
+                    ))}
+                </Row>
+
+            </Container>
         </div>
     );
 }
