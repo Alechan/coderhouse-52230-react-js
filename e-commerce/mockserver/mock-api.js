5a5ad9c0473cfe2ce1c47a8d2aa77fb0b590becc
Alechan <noemail@example.com>
2023-06-09 15:41:50 -0300

    add 2s delay to mock server

--- e-commerce/mockserver/mock-api.js
+++ e-commerce/mockserver/mock-api.js
@@ -3,6 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('mockserver/db.json');
 const middlewares = jsonServer.defaults();
 
+// Wait 2s in all requests
+server.use((req, res, next) => {
+    let seconds = 1000;
+    setTimeout(next, 2* seconds); // 2-second delay
+});
+
 server.use(middlewares);
 server.use(router);
 server.listen(3001, () => {
