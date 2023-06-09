374d26b5d8bc14779b0697ea1ff78a2c464bdb37
Alechan <noemail@example.com>
2023-06-09 16:29:43 -0300

    minor comment fix

--- e-commerce/src/components/body/itemcount/ItemCount.jsx
+++ e-commerce/src/components/body/itemcount/ItemCount.jsx
@@ -10,7 +10,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
             // For counters, it's better to use a function to reference the previous value instead of passing the current value
             // https://kentcdodds.com/blog/use-state-lazy-initialization-and-function-updates
             setCount(prevCount => prevCount + 1);
-            // TODO: No entiendo acá si rompemos con el artículo que pegué arriba, o si es que no se aplica en este caso
+            // TODO: No entiendo si haciendo onAdd con numerito en vez de con función rompo con el artículo que pegué arriba
             onAdd(count + 1)
         }
     };
@@ -23,9 +23,8 @@ const ItemCount = ({stock, initial, onAdd}) => {
 
     return (
         <div>
-            <ButtonGroup type="checkbox" defaultValue={[1, 3]} className="mb-2">
+            <ButtonGroup type="checkbox" className="mb-2">
                 <Button size="sm" variant="secondary" onClick={decrease}>-</Button>
-                {/*Center the span */}
                 <span className="item-count-n">{count}</span>
                 <Button size="sm" variant="secondary" onClick={increase}>+</Button>
             </ButtonGroup>
