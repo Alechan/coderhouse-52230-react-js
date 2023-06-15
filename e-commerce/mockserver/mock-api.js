598e94aec080e42d36ad68f9df715205f2a81830
Alechan <noemail@example.com>
2023-06-14 23:31:54 -0300

    add assets to mockserver

--- e-commerce/mockserver/mock-api.js
+++ e-commerce/mockserver/mock-api.js
@@ -1,12 +1,19 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('mockserver/db.json');
+var path = require('path')
+var express = require('express')
+var jsonServer = require('json-server')
+var demodata = require('./db.json')
 const middlewares = jsonServer.defaults();
 
-// Wait 2s in all requests
+var router = jsonServer.router(demodata) // Express router
+var server = jsonServer.create()       // Express server
+
+server.use('/assets', express.static(path.join(__dirname, 'assets')))
+server.use(router)
+
+// Add "synthetic" delay to all requests
 server.use((req, res, next) => {
     let seconds = 1000;
-    setTimeout(next, 2* seconds); // 2-second delay
+    setTimeout(next, 3* seconds);
 });
 
 server.use(middlewares);
@@ -14,4 +21,3 @@ server.use(router);
 server.listen(3001, () => {
     console.log('JSON Server is running');
 });
-
