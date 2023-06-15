189a5c90b827c82c11ad68d9ac1e64e4c2179cf2
Alechan <noemail@example.com>
2023-06-15 00:22:43 -0300

    add images to each item

--- e-commerce/src/services/items/index.jsx
+++ e-commerce/src/services/items/index.jsx
@@ -1,6 +1,14 @@
 const getItems = async () => {
     const response = await fetch('http://localhost:3001/items');
-    return await response.json();
+    const items = await response.json();
+
+    // Pre-pend the mockserver url to each image
+    return items.map(item => {
+        return {
+            ...item,
+            pictureUrl: `http://localhost:3001${item.pictureUrl}`
+        };
+    });
 }
 
 const getItem = async (id) => {
