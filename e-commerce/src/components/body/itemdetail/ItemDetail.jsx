bd96e25fee79bdfc2287e2e3e970fdecd2d3a690
Alechan <noemail@example.com>
2023-06-28 22:10:01 -0300

    show modal on "add to cart" click

--- e-commerce/src/components/body/itemdetail/ItemDetail.jsx
+++ e-commerce/src/components/body/itemdetail/ItemDetail.jsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from "react";
 import ItemCount from "../itemcount/ItemCount";
 import './ItemDetail.css';
 
-import {Card, Spinner} from "react-bootstrap";
+import {Button, Card, Modal, Spinner} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 function ItemDetail({item}) {
     const [imageLoaded, setImageLoaded] = useState(false);
     const [img, setImg] = useState();
+    const [shouldShowModal, setShouldShowModal] = useState(false);
 
     // TODO: no pude hacer andar el spinner con un <img onload>, así que hago el fetch a manopla nomás
     const fetchImage = async (url) => {
@@ -17,40 +19,59 @@ function ItemDetail({item}) {
 
     // Fetch the image when the component mounts
     useEffect(() => {
-        fetchImage(item.pictureUrl)
-            .then(imageFetched => {
-                setImg(imageFetched);
-                setImageLoaded(true);
-            })
-            // Ignore errors
-            .catch(() => {
-            });
-    },
+            fetchImage(item.pictureUrl)
+                .then(imageFetched => {
+                    setImg(imageFetched);
+                    setImageLoaded(true);
+                })
+                // Ignore errors
+                .catch(() => {
+                });
+        },
         []
     )
 
     // TODO: Todavía no definimos qué va a hacer onAdd
     const onAdd = (count) => {
-        console.log("Agregaste " + count + " '" + item.title + "' al carrito");
+        setShouldShowModal(true);
     }
     return (
-        <Card className="item-detail">
-            <Card.Body>
-                {!imageLoaded ?
-                    <Spinner animation="border" role="status" className="ml-2"/>
-                    : <Card.Img variant="top" src={img} />
-                }
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                    {item.cityName}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-between align-items-center">
-                <p>${item.price}</p>
-                <p>Stock: {item.stock}</p>
-                <ItemCount stock={item.stock} initial={0} onAdd={onAdd}/>
-            </Card.Footer>
-        </Card>
+        <>
+            <Card className="item-detail">
+                <Card.Body>
+                    {!imageLoaded ?
+                        <Spinner animation="border" role="status" className="ml-2"/>
+                        : <Card.Img variant="top" src={img}/>
+                    }
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>
+                        {item.cityName}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="d-flex justify-content-between align-items-center">
+                    <p>${item.price}</p>
+                    <p>Stock: {item.stock}</p>
+                    <ItemCount stock={item.stock} initial={0} onAdd={onAdd}/>
+                </Card.Footer>
+            </Card>
+            <Modal className="justify-content-center align-items-center" centered show={shouldShowModal}>
+                <Modal.Body>
+                    <Modal.Body>
+                        <div className="text-center">
+                            <p>¡Ítems agregados al carrito!</p>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <LinkContainer to="/" className="modal-home-button">
+                                <Button>Seguir comprando</Button>
+                            </LinkContainer>
+                            <LinkContainer to="/cart" className="modal-cart-button">
+                                <Button>Ir al carrito</Button>
+                            </LinkContainer>
+                        </div>
+                    </Modal.Body>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 }
 
