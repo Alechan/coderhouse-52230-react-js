698153a70b861a80ff8ee59c5445f02f6d6d6215
Alechan <noemail@example.com>
2023-07-01 14:46:54 -0300

    hide navbar cart when no items in it

--- e-commerce/src/components/navbar/NavbarCartWidget.jsx
+++ e-commerce/src/components/navbar/NavbarCartWidget.jsx
@@ -1,8 +1,6 @@
 import {Cart4} from "react-bootstrap-icons";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap";
-import {CartContext} from "../../context/cart";
-import {useContext} from "react";
 
 const NavbarCartWidget = ({nItems}) => {
     return (
