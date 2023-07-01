ce809c1823660bdc855060a6d4a27edaac19b7c3
Alechan <noemail@example.com>
2023-07-01 18:16:40 -0300

    minor changes and bugfixes in /cart

--- e-commerce/src/components/body/cartcontainer/CartContainer.jsx
+++ e-commerce/src/components/body/cartcontainer/CartContainer.jsx
@@ -4,13 +4,17 @@ import React, {useContext, useState} from "react";
 import ItemCountCart from "../itemcountcart/ItemCountCart";
 import {Button, Modal} from "react-bootstrap";
 import './CartContainer.scss'
+import {LinkContainer} from "react-router-bootstrap";
+import {ROUTES} from "../../../constants";
 
 const CartContainer = () => {
     const {cart, addItemToCart, removeItemFromCart, getTotalPrice} = useContext(CartContext);
     const [itemTryingToReduceToZero, setItemTryingToReduceToZero] = useState(null);
 
-    const handleIncreaseQuantity = (item) => {
-        addItemToCart(item, 1);
+    const handleIncreaseQuantity = (item, prevQuantity) => {
+        if (prevQuantity < item.stock) {
+            addItemToCart(item, 1);
+        }
     };
 
     const handleReduceQuantity = (item, prevQuantity) => {
@@ -35,62 +39,63 @@ const CartContainer = () => {
 
     return (
         <div >
-            <h1 className="text-center">¡Tenés estos ítems en tu carrito!</h1>
             {
                 cart.length === 0 ?
-                    <p>Your cart is empty.</p>
+                    <div>
+                        <h1>Tu carrito está vacío :(</h1>
+                        <LinkContainer to={ROUTES.HOME} className="d-block">
+                            <Button>Ir a llenar al carrito</Button>
+                        </LinkContainer>
+                    </div>
                     :
-                    <Table striped hover borderless style={{borderRadius: "10px"}}>
-                        <tbody>
-                        {cart.map(({item, quantity}, index) => {
-                                return <tr>
-                                    {/* Show index to let the buyer know how many unique items are in the cart*/}
-                                    <td className="align-middle">
-                                        <p className="m-1">
-                                            {index + 1}
-                                        </p>
-                                    </td>
-                                    <td className="align-middle">
-                                        <p className="m-1">
-                                            {item.title}
-                                        </p>
-                                    </td>
-                                    {/* //TODO: cuando fetcheo los ítems aprovechar y fetchear la imagen así no tengo que fetchearla en todos lados */}
-                                    <td className="align-middle">
-                                        <p className="m-1">
-                                            {item.pictureUrl}
-                                        </p>
-                                    </td>
-                                    <td className="align-middle">
-                                        <p className="m-1">
-                                            {item.cityName}
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <div className="d-flex justify-content-center align-items-center m-1">
-                                            <p className="m-1">${item.price}</p>
-                                            <p className="m-1">Stock: {item.stock}</p>
-                                            <ItemCountCart
-                                                count={quantity}
-                                                onDecrease={() => handleReduceQuantity(item, quantity)}
-                                                onIncrease={() => handleIncreaseQuantity(item)}
-                                                stock={item.stock}
-                                                onRemove={() => setItemTryingToReduceToZero(item)}
-                                            />
-                                        </div>
-                                    </td>
-                                </tr>
+                    <>
+                        <h1 className="text-center">¡Tenés estos ítems en tu carrito!</h1>
+                        <Table striped hover borderless className="text-center">
+                            <tbody>
+                            {cart.map(({item, quantity}) => {
+                                    return <tr>
+                                        <td className="align-middle">
+                                            <p className="m-1">
+                                                {item.title}
+                                            </p>
+                                        </td>
+                                        {/* //TODO: cuando fetcheo los ítems aprovechar y fetchear la imagen así no tengo que fetchearla en todos lados */}
+                                        <td className="align-middle">
+                                            <p className="m-1">
+                                                {item.pictureUrl}
+                                            </p>
+                                        </td>
+                                        <td className="align-middle">
+                                            <p className="m-1">
+                                                {item.cityName}
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <div className="d-flex justify-content-center align-items-center m-1">
+                                                <p className="m-1">${item.price}</p>
+                                                <p className="m-1">Stock: {item.stock}</p>
+                                                <ItemCountCart
+                                                    count={quantity}
+                                                    onDecrease={() => handleReduceQuantity(item, quantity)}
+                                                    onIncrease={() => handleIncreaseQuantity(item, quantity)}
+                                                    stock={item.stock}
+                                                    onRemove={() => setItemTryingToReduceToZero(item)}
+                                                />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                }
+                            )
                             }
-                        )
-                        }
-                        <tr>
-                            <td colSpan="5" className="align-middle align-items-center justify-content-center text-center">
+                            <tr>
+                                <td colSpan="4" className="align-middle align-items-center justify-content-center text-center">
                                     Total: ${getTotalPrice()}
 
-                            </td>
+                                </td>
                             </tr>
-                        </tbody>
-                    </Table>
+                            </tbody>
+                        </Table>
+                    </>
             }
             <Modal className="justify-content-center align-items-center" centered show={itemTryingToReduceToZero}>
                 <Modal.Body>
