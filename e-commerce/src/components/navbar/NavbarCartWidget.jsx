9f57a83ed89fd0f5f40591fba3f9561ff796d497
Alechan <noemail@example.com>
2023-07-03 00:32:36 -0300

    visual tweaks in navbar

--- e-commerce/src/components/navbar/NavbarCartWidget.jsx
+++ e-commerce/src/components/navbar/NavbarCartWidget.jsx
@@ -1,16 +1,22 @@
 import {Cart4} from "react-bootstrap-icons";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap";
+import {useState} from "react";
 
 const NavbarCartWidget = ({nItems}) => {
+    const [cartColor, setCartColor] = useState("white");
+
     return (
         <>
             {
                 nItems > 0 ?
                     <LinkContainer to="/cart">
-                        <Nav.Link>
-                            <Cart4 color="white"/>
-                            <span className="span-cart-widget">{nItems}</span>
+                        <Nav.Link
+                            onMouseEnter={() => setCartColor("lightgreen")}
+                            onMouseLeave={() => setCartColor("white")}
+                        >
+                            <Cart4 color={cartColor}/>
+                            <span style={{color: cartColor}}>{nItems}</span>
                         </Nav.Link>
                     </LinkContainer>
                     : null
