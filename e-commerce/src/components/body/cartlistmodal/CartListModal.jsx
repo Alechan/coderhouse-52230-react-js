c4cb529b3f6758d9e8cd508c6883d1ceb4f1928b
Alechan <noemail@example.com>
2023-07-05 22:27:42 -0300

    refactor scss used in many places

--- e-commerce/src/components/body/cartlistmodal/CartListModal.jsx
+++ e-commerce/src/components/body/cartlistmodal/CartListModal.jsx
@@ -2,7 +2,6 @@ import {Button, Modal} from "react-bootstrap";
 import React from "react";
 
 const CartListModal = ({shouldShowModal, pMsg, buttons}) => {
-    const buttonClassName = "cart-modal-button";
 
     return (<Modal className="justify-content-center align-items-center" centered show={shouldShowModal}>
             <Modal.Body>
@@ -13,7 +12,7 @@ const CartListModal = ({shouldShowModal, pMsg, buttons}) => {
                     <div className="d-flex justify-content-center">
                         {
                             buttons.map((button) => {
-                                return <Button variant={button.variant} onClick={button.onClick} className={buttonClassName}>
+                                return <Button variant={button.variant} onClick={button.onClick} className="button-with-margin">
                                     {button.text}
                                 </Button>
                             })
