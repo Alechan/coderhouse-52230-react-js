b68af65a2c3b6c9e80fcfcf1d916b01fb4b292c6
Alechan <noemail@example.com>
2023-07-07 18:07:12 -0300

    fix key in element inside map in ModalWithPAndButtons.jsx

--- e-commerce/src/components/body/modalwithpandbuttons/ModalWithPAndButtons.jsx
+++ e-commerce/src/components/body/modalwithpandbuttons/ModalWithPAndButtons.jsx
@@ -6,20 +6,19 @@ const ModalWithPAndButtons = ({shouldShowModal, pMsg, buttons}) => {
     return (
         <Modal className="justify-content-center align-items-center" centered show={shouldShowModal}>
             <Modal.Body>
-                <Modal.Body>
-                    <div className="text-center">
-                        <p>{pMsg}</p>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        {
-                            buttons.map((button) => {
-                                return <Button variant={button.variant} onClick={button.onClick} className="button-with-margin">
-                                    {button.text}
-                                </Button>
-                            })
-                        }
-                    </div>
-                </Modal.Body>
+                <div className="text-center">
+                    <p>{pMsg}</p>
+                </div>
+                <div className="d-flex justify-content-center">
+                    {
+                        buttons.map((button, index) => {
+                            return <Button key={index} variant={button.variant} onClick={button.onClick}
+                                           className="button-with-margin">
+                                {button.text}
+                            </Button>
+                        })
+                    }
+                </div>
             </Modal.Body>
         </Modal>
     );
